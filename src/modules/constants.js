9fcf9b0bd309727337e7d3b74411691b4ed6c135
belaymit <2belamit@gmail.com>
2023-01-03 13:22:12 +0300

    added base URL to constants

--- src/modules/constants.js
+++ src/modules/constants.js
@@ -1,2 +1,5 @@
 export const navToggle = document.querySelector('.nav-toggle');
 export const navLinks = document.querySelector('.links');
+export const movieURL = 'https://api.tvmaze.com/search/shows?q=';
+export const movieDetail = document.querySelector('.box-container');
+export const shownMovies = document.querySelector('.shown-movies');
