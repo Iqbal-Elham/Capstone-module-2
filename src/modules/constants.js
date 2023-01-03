a2294389f9ca0dd9df996f2130e3474bad8bd1d9
belaymit <2belamit@gmail.com>
2023-01-03 15:34:45 +0300

    declared popup closer to constants

--- src/modules/constants.js
+++ src/modules/constants.js
@@ -1,5 +1,7 @@
 export const navToggle = document.querySelector('.nav-toggle');
 export const navLinks = document.querySelector('.links');
-export const movieURL = 'https://api.tvmaze.com/search/shows?q=';
 export const movieDetail = document.querySelector('.box-container');
 export const shownMovies = document.querySelector('.shown-movies');
+export const movieCloseBtn = document.querySelector('#movie-close-btn');
+export const popupSection = document.querySelector('.popup-section');
+export const movieURL = 'https://api.tvmaze.com/search/shows?q=';
