946c56ad2719ef0c051409143e7bbc217db6c6e3
belaymit <2belamit@gmail.com>
2023-01-03 18:24:18 +0300

    added packed files

---
+++ dist/images/index09a8f0dda1.js
@@ -0,0 +1,1 @@
+import"./style.css";import showMenuItems from"./modules/eventHandler.js";import populateData from"./modules/populateData.js";showMenuItems(),populateData();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import './style.css';
 import { showMenuItems, closePopup } from './modules/eventHandler.js';
 import populateData from './modules/populateData.js';
+import './style.css';
 
 showMenuItems();
 closePopup();
