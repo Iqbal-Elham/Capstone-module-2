941371a80cc9fb649e25c11b0d4631518828e0fb
iqbal-elham <iqbal.elham7@gmail.com>
2023-01-04 21:32:10 +0430

    Fix linter Errors

---
+++ dist/images/index904bb95749.js
@@ -0,0 +1,1 @@
+import"./style.css";import{showMenuItems,closePopup,closeReservation}from"./modules/eventHandler.js";import populateData from"./modules/populateData.js";populateData(),showMenuItems(),closePopup(),closeReservation();
